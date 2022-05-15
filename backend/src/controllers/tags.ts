46d37dfc8ab00582d231b3f3efea078f859724b2
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2022-05-15 19:17:34 +0200

    get tags

---
+++ backend/src/controllers/tags.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express';
+import { prisma } from '../../prisma/client';
+
+export const getTagList = async (req: Request, res: Response) => {
+  const tags = await prisma.tag.findMany({});
+  res.send({ tags: tags.map(({ name }) => name) });
+};
--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { articlesRouter } from './articles';
 import { profilesRouter } from './profiles';
+import { tagsRouter } from './tags';
 import { userRouter } from './user';
 import { authRouter } from './users';
 const apiRouter = Router();
@@ -9,5 +10,6 @@ apiRouter.use('/users', authRouter);
 apiRouter.use('/user', userRouter);
 apiRouter.use('/profiles', profilesRouter);
 apiRouter.use('/articles', articlesRouter);
+apiRouter.use('/tags', tagsRouter);
 
 export { apiRouter };
